fac54a524b613ee5f30fdd9983f583a284a00729
Antigoni Makri <makanti@gmail.com>
2021-05-09 16:13:45 +0100

    convert class components to functional components

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,33 +1,26 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./Balance.css";
 
-class Balance extends Component {
-  constructor() {
-    super();
-    this.state = {
-      alternateCurrency: "USD"
-    };
-  }
-
-  render() {
-    return (
-      <div className="Balance">
-        <h2 className="Balance-title">
-          Your account balance is
-          <span className="Balance-total">£{this.props.total}</span>
-        </h2>
-        <div className="Balance-alt">
-          Your balance is ??? in
-          <select defaultValue={this.state.alternateCurrency}>
-            {this.props.currencies.map((currency, index) => (
-              <option key={index}>{currency}</option>
-            ))}
-          </select>
-          .
-        </div>
+const Balance = (props) => {
+  const [alternateCurrency] = useState("USD");
+  const { total, currencies } = props;
+  return (
+    <div className="Balance">
+      <h2 className="Balance-title">
+        Your account balance is
+        <span className="Balance-total">£{total}</span>
+      </h2>
+      <div className="Balance-alt">
+        Your balance is ??? in
+        <select defaultValue={alternateCurrency}>
+          {currencies.map((currency, index) => (
+            <option key={index}>{currency}</option>
+          ))}
+        </select>
+        .
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Balance;
--- src/components/CalcPayment.js
+++ src/components/CalcPayment.js
@@ -1,42 +1,34 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 import "./CalcPayment.css";
 
-class Payment extends Component {
-  constructor() {
-    super();
-    this.state = {
-      selectedCurrency: "USD",
-      amount: 0
-    };
-  }
+const Payment = (props) => {
+  const [selectedCurrency, setSelectedCurrency] = useState("USD");
+  const [amount, setAmount] = useState("USD");
 
-  electCurrency = (event) => {
+  const selectCurrency = (event) => {
     const currency = event.target.value;
-    this.setState({
-      selectedCurrency: currency
-    });
+    setSelectedCurrency(currency);
   };
 
-  render() {
-    return (
-      <div className="CalcPayment">
-        <h2 className="CalcPayment-label">Calculate Payment in GBP</h2>
-        <div className="CalcPayment-control">
-          <select onChange={this.selectCurrency} defaultValue={this.state.selectedCurrency}>
-            {this.props.currencies.map((currency, index) => (
-              <option key={index}>{currency}</option>
-            ))}
-          </select>
-          <input className="CalcPayment-amount" type="text" defaultValue="0.00" />
-          is worth <span className="CalcPayment-result">???</span> in GBP.
-          <div className="CalcPayment-calculate">
-            <Button>Calculate</Button>
-          </div>
+  const { currencies } = props;
+  return (
+    <div className="CalcPayment">
+      <h2 className="CalcPayment-label">Calculate Payment in GBP</h2>
+      <div className="CalcPayment-control">
+        <select onChange={selectCurrency} defaultValue={selectedCurrency}>
+          {currencies.map((currency, index) => (
+            <option key={index}>{currency}</option>
+          ))}
+        </select>
+        <input className="CalcPayment-amount" type="text" defaultValue="0.00" />
+        is worth <span className="CalcPayment-result">???</span> in GBP.
+        <div className="CalcPayment-calculate">
+          <Button>Calculate</Button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Payment;
