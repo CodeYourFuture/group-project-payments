db6423770253d8f2845dd7a936296e4bb6ab51de
Oksana Kostenko <kseniavlad31@gmail.com>
2021-05-10 22:03:17 +0100

    Revert "fixed typo currency => date"
    This reverts commit 0a083c8f55d4293acec96d878802625a3f21c87b.

--- src/data/payments.js
+++ src/data/payments.js
@@ -1,34 +1,34 @@
 let payments = [
   {
-    date: "2018-05-12",
+    currency: "2018-05-12",
     currency: "GBP",
     amount: 6.89,
     description: "Dinner with friends at a local restaurant",
     status: "Pending"
   },
   {
-    date: "2018-02-24",
+    currency: "2018-02-24",
     currency: "USD",
     amount: 12.23,
     description: "New headphones purchased from Amazon with free delivery",
     status: "Complete"
   },
   {
-    date: "2017-12-30",
+    currency: "2017-12-30",
     currency: "AUD",
     amount: 28.74,
     description: "Groceries for the week",
     status: "Complete"
   },
   {
-    date: "2017-10-17",
+    currency: "2017-10-17",
     currency: "GBP",
     amount: 2.06,
     description: "Coffee with Luke at iCafe",
     status: "Complete"
   },
   {
-    date: "2017-09-02",
+    currency: "2017-09-02",
     currency: "TRY",
     amount: 87,
     description: "Gift for my mother back home",
