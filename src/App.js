1f9707521e52b4bfaae5447b4ec4bd9480ae70ab
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-05-12 09:57:37 +0100

    organized the code a bit better

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from "react";
+import React from "react";
 import Balance from "./components/Balance";
 import CalcPayment from "./components/CalcPayment";
 import Payments from "./components/Payments";
@@ -7,28 +7,13 @@ import "./App.css";
 
 const App = () => {
   const balance = 87.43;
-  const [fromGBPto, setFromGBPto] = useState("USD");
-  const [convertedValue, setConvertedValue] = useState("Loading");
-
-  useEffect(() => {
-    fetch(`https://api.frankfurter.app/latest?amount=${balance}&from=GBP&to=${fromGBPto}`)
-      .then((res) => res.json())
-      .then((data) => setConvertedValue(data.rates[fromGBPto]))
-      .catch((err) => console.error(err));
-  }, [fromGBPto]);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="App-title">Payments</h1>
       </header>
-      <Balance
-        total={balance}
-        currencies={currencies}
-        setFromGBPto={setFromGBPto}
-        convertedValue={convertedValue}
-        fromGBPto={fromGBPto}
-      />
+      <Balance total={balance} currencies={currencies} />
       <CalcPayment currencies={currencies} />
       <h2>Payments</h2>
       <Payments />
--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,27 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Balance.css";
 
-const Balance = ({ total, currencies, setFromGBPto, convertedValue, fromGBPto }) => {
+const Balance = ({ total, currencies }) => {
+  const [convertTo, setConvertTo] = useState("USD");
+  const [convertedValue, setConvertedValue] = useState("Loading");
+  const convertFrom = "GBP";
+
+  useEffect(() => {
+    fetch(`https://api.frankfurter.app/latest?amount=${total}&from=${convertFrom}&to=${convertTo}`)
+      .then((res) => res.json())
+      .then((data) => setConvertedValue(data.rates[convertTo]))
+      .catch((err) => console.error(err));
+  }, [convertTo]);
+
   return (
-    <div className="Balance">
+    <div className="total">
       <h2 className="Balance-title">
         Your account balance is
         <span className="Balance-total">£{total}</span>
       </h2>
       <div className="Balance-alt">
         Your balance is {convertedValue} in
-        <select defaultValue={fromGBPto}>
-          {currencies.map((currency, index) => (
-            <option
-              key={index}
-              onClick={() => {
-                if (currency !== "GBP") {
-                  setFromGBPto(currency);
-                }
-              }}>
-              {currency}
-            </option>
-          ))}
+        <select defaultValue={convertTo}>
+          {currencies
+            .filter((elem) => elem !== "GBP")
+            .map((currency, index) => (
+              <option
+                key={index}
+                onClick={() => {
+                  setConvertTo(currency);
+                }}>
+                {currency}
+              </option>
+            ))}
         </select>
       </div>
     </div>
