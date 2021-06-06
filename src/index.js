fb491b2785ce019b2cae04ab769ab375bfc0b176
yunusfirat <yunusfirat44@gmail.com>
2021-06-06 19:05:11 +0100

    context js created

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import { AppProvider } from './context'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <React.StrictMode>
+      <AppProvider>
+      <App />
+      </AppProvider>
+    </React.StrictMode>,
+    document.getElementById("root"),
+  );
+  
 registerServiceWorker();
